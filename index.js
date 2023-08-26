3837191065d2e8e61e217127228861a855f6bf5c
Polichinell <tsuqinoa@gmail.com>
2023-08-26 18:37:49 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+const submitData = (userName, userMail) => {
+    const formData = {
+        name: userName,
+        email: userMail,
+      };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.querySelector('body').append(object.id);
+      })
+      .catch(function (error) {
+        alert("Oopsie");
+        console.log(error.message);
+        document.querySelector('body').append(error.message)
+      })
+}
